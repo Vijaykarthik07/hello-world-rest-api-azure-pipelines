bf7034d1eb6d1d96954a899dfba15eed4881c69a
Vijaykarthik <vijay.karthik@bpmlinks.com>
2020-05-27 09:43:39 +0530

    updated the Controller File

--- src/main/java/com/vijaykarthik/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/vijaykarthik/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World From Azure DevOps Deployment";
+		return "Hello World From Azure DevOps Deployment!!";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
